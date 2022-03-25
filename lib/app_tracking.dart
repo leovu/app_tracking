cf1b9a711c3db967eee37165618ed65012194540
Tran Ly <lytran997@gmail.com>
2022-03-25 14:29:04 +0700

    push tracking web

--- lib/app_tracking.dart
+++ lib/app_tracking.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
+// import 'package:flutter/foundation.dart' show kIsWeb;
 
 class AppTracking {
   static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
@@ -19,22 +19,14 @@ class AppTracking {
   //TODO: setup in main.dart, input MyApp parameter. You have to put WidgetsFlutterBinding.ensureInitialized(); before init
   static init({required Widget myApp, required bool testingCrashlytics}) async {
     await Firebase.initializeApp();
-    if(!kIsWeb) {
-      _kTestingCrashlytics = testingCrashlytics;
-    }
-    if(kIsWeb) {
-        runApp(myApp);
-     }
-    else {
-        runZonedGuarded(() {
-        _initCrashlytics();
-        runApp(myApp);
-      }, FirebaseCrashlytics.instance.recordError);
-    }
+    _kTestingCrashlytics = testingCrashlytics;
+    runZonedGuarded(() {
+      _initCrashlytics();
+      runApp(myApp);
+    }, FirebaseCrashlytics.instance.recordError);
   }
 
   static _initCrashlytics() async {
-    if (kIsWeb) return;
     await FirebaseCrashlytics.instance
         .setCrashlyticsCollectionEnabled(_kTestingCrashlytics);
     Function(FlutterErrorDetails)? originalOnError = FlutterError.onError;
@@ -48,13 +40,12 @@ class AppTracking {
 
   static trackingErrorAPI(
       {required String userId,
-      required String fullName,
-      required String uuid,
-      required String url,
-      required dynamic head,
-      required dynamic params,
-      required dynamic messageError}) async {
-    if (kIsWeb) return;
+        required String fullName,
+        required String uuid,
+        required String url,
+        required dynamic head,
+        required dynamic params,
+        required dynamic messageError}) async {
     dynamic log = params;
     String error = "";
     try {
@@ -68,7 +59,7 @@ class AppTracking {
       error = e.toString();
     }
     analytics.logEvent(name: 'api_tracking', parameters: <String, dynamic>{
-      "url": url ?? "",
+      "url": url,
       "headers": head != null ? "${head.toString()}" : "",
       "params": params != null ? "${log.toString()}" : "",
       "error": error,
@@ -80,10 +71,9 @@ class AppTracking {
 
   static trackingScreen(
       {required String screenName,
-      required String userId,
-      required String fullName,
-      required String uuid}) {
-    if (kIsWeb) return;
+        required String userId,
+        required String fullName,
+        required String uuid}) {
     analytics.logEvent(
       name: 'screen_tracking',
       parameters: <String, dynamic>{
@@ -98,9 +88,8 @@ class AppTracking {
 
   static trackingNotification(
       {required String userId,
-      required String fullName,
-      required dynamic params}) {
-    if (kIsWeb) return;
+        required String fullName,
+        required dynamic params}) {
     dynamic log = params;
     try {
       if (!(log is String)) {
@@ -117,7 +106,7 @@ class AppTracking {
     });
   }
 
-  static trackingAPIDetection(
+  static trackingAPIOCRSuccess(
       {required String userId,
         required String fullName,
         required String uuid,
@@ -126,7 +115,6 @@ class AppTracking {
         required dynamic head,
         required dynamic params,
         required dynamic messageError}) async {
-    if (kIsWeb) return;
     dynamic log = params;
     String error = "";
     try {
@@ -139,7 +127,40 @@ class AppTracking {
     } catch (e) {
       error = e.toString();
     }
-    analytics.logEvent(name: 'API_OCR', parameters: <String, dynamic>{
+    analytics.logEvent(name: 'API_OCR_SUCCESS', parameters: <String, dynamic>{
+      "url": url,
+      "headers": head != null ? "${head.toString()}" : "",
+      "params": params != null ? "${log.toString()}" : "",
+      "message": error,
+      "userId": userId,
+      "fullName": fullName,
+      "uuid": uuid,
+      "status": status
+    });
+  }
+
+  static trackingAPIOCRFailed(
+      {required String userId,
+        required String fullName,
+        required String uuid,
+        required String url,
+        required String status,
+        required dynamic head,
+        required dynamic params,
+        required dynamic messageError}) async {
+    dynamic log = params;
+    String error = "";
+    try {
+      if (!(log is String)) {
+        if (log.containsKey('password')) {
+          log.remove('password');
+        }
+      }
+      error = messageError.toString();
+    } catch (e) {
+      error = e.toString();
+    }
+    analytics.logEvent(name: 'API_OCR_FAILED', parameters: <String, dynamic>{
       "url": url,
       "headers": head != null ? "${head.toString()}" : "",
       "params": params != null ? "${log.toString()}" : "",
